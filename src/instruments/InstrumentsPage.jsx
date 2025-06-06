54bdbe6e554fcb1704a240cbfea5c275e56b806f
Ali Samadi <alisamadi.096@gmail.com>
2025-06-05 23:00:29 -0500

    Initial React Components were created for the Frontend part, #1

---
+++ src/instruments/InstrumentsPage.jsx
@@ -0,0 +1,9 @@
+import InstrumentsList from "./InstrumentsList.jsx";
+export default function InstrumentsPage() {
+  return (
+    <main>
+      <h1>Catalog</h1>
+      <InstrumentsList />
+    </main>
+  );
+}
