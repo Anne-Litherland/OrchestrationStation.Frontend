e601206843746e701b663b91ee22302a51a13766
Anne Litherland <litherlandanne@gmail.com>
2025-06-09 16:02:04 -0400

    Javascript for feedback form created

--- src/instruments/InstrumentsPage.jsx
+++ src/instruments/InstrumentsPage.jsx
@@ -1,6 +1,6 @@
 import InstrumentsList from "./InstrumentsList.jsx";
-import { Link } from "react-router";
 import { useAuth } from "../auth/AuthContext.jsx";
+import Feedback from "../userInput/feedback.jsx";
 
 export default function InstrumentsPage() {
   const { token } = useAuth();
@@ -8,24 +8,7 @@ export default function InstrumentsPage() {
     <main>
       <h1>Catalog</h1>
       <InstrumentsList />
-      {token ? (
-        <form>
-          <label>
-            Username
-            <input type="text" name="username" />
-          </label>
-          <label>
-            Message
-            <input type="text" name="feedback" />
-          </label>
-          <button>Submit</button>
-          {error && <output>{error}</output>}
-        </form>
-      ) : (
-        <Link to="/register" id="link">
-          Sign up to send us a message!
-        </Link>
-      )}
+      <Feedback />
     </main>
   );
 }
---
+++ src/userInput/feedback.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../auth/AuthContext";
+
+export default function Feedback() {
+  const navigate = useNavigate();
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+  const onSubmit = async (formData) => {
+    const username = formData.get("username");
+    const message = formData.get("message");
+    try {
+      await submit({ username, message });
+      navigate("/");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <form action={onSubmit}>
+      <label>
+        Username
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Message
+        <input type="text" name="message" required />
+      </label>
+      <button>Submit</button>
+      {error && <output>{error}</output>}
+    </form>
+  );
+}
